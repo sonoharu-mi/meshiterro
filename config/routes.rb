c93c67fb42da7075efc609e6ba14bd3a4048c73a
sonoharu-mi <215863048+sonoharu-mi@users.noreply.github.com>
2025-08-27 12:29:41 +0000

    admin_認証機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :password], controllers: {sessions: 'admin/sessions'}
   root to: "homes#top"
   devise_for :users
   get "/homes/about" => "homes#about", as: "about"
